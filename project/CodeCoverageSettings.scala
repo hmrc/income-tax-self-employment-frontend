5d10fecc78606baa47d3630fecbb19a4435c81ac
Connie Bernardin <97225150+conniebernardin@users.noreply.github.com>
2023-07-26 15:08:50 +0100

    SASS-5199 income-tax-self-employment-frontend microservice setup (#1)
    * SASS-5199 setup
    
    * SASS-5199 wip
    
    ---------
    
    Co-authored-by: conniebernardin <conniebernardin@users.noreply.github.com>

---
+++ project/CodeCoverageSettings.scala
@@ -0,0 +1,26 @@
+import sbt.Setting
+import scoverage.ScoverageKeys
+
+object CodeCoverageSettings {
+
+  private val excludedPackages: Seq[String] = Seq(
+    "<empty>",
+    "Reverse.*",
+    "uk.gov.hmrc.BuildInfo",
+    "app.*",
+    "prod.*",
+    ".*Routes.*",
+    "testOnly.*",
+    "testOnlyDoNotUseInAppConf.*"
+  )
+
+  val settings: Seq[Setting[_]] = Seq(
+    ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(";"),
+    ScoverageKeys.coverageExcludedFiles := "<empty>;Reverse.*;.*handlers.*;.*components.*;" +
+      ".*Routes.*;.*viewmodels.govuk.*;",
+    ScoverageKeys.coverageMinimumStmtTotal := 78,
+    ScoverageKeys.coverageFailOnMinimum := true,
+    ScoverageKeys.coverageHighlighting := true
+  )
+
+}
