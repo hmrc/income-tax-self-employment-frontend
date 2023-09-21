cb8753945078c6001174f55f47021d04f7d13314
edem-nsefik-hmrc <edem.nsefik2@digital.hmrc.gov.uk>
2023-09-21 14:11:37 +0100

    SASS-5650: Authorisation implementation
    Added User to DataRequest

--- app/models/requests/DataRequest.scala
+++ app/models/requests/DataRequest.scala
@@ -16,9 +16,10 @@
 
 package models.requests
 
+import controllers.actions.AuthenticatedIdentifierAction.User
 import play.api.mvc.{Request, WrappedRequest}
 import models.UserAnswers
 
-case class OptionalDataRequest[A] (request: Request[A], userId: String, userAnswers: Option[UserAnswers]) extends WrappedRequest[A](request)
+case class OptionalDataRequest[A] (request: Request[A], userId: String, user: User, userAnswers: Option[UserAnswers]) extends WrappedRequest[A](request)
 
-case class DataRequest[A] (request: Request[A], userId: String, userAnswers: UserAnswers) extends WrappedRequest[A](request)
+case class DataRequest[A] (request: Request[A], userId: String, user: User, userAnswers: UserAnswers) extends WrappedRequest[A](request)
