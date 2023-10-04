146834fec65b9f60e96dd52377a4db09140edf91
Jacob Raffe <jakeraffe24@gmail.com>
2023-10-04 13:23:23 +0100

    [SASS-5276-5552] Edit conditional journey statuses and URLs

--- app/models/viewModels/TradeJourneyStatusesViewModel.scala
+++ app/models/viewModels/TradeJourneyStatusesViewModel.scala
@@ -37,26 +37,41 @@ object TradeJourneyStatusesViewModel {
 
   def buildSummaryList(business: TradesJourneyStatuses, taxYear: Int)(implicit messages: Messages): SummaryList = {
 
-    val status = (journey: Journey) => {
+    val getStatus = (journey: Journey) => {
       val optJourney = business.journeyStatuses.filter(_.journey.equals(journey))
       if (optJourney.isEmpty) None else optJourney.head.completedState
     }
 
-    val (abroadStatus, incomeStatus, expensesStatus, nationalInsuranceStatus) =
-      (status(Abroad), status(Income), status(Expenses), status(NationalInsurance))
+    val (abroadCompletionStatus, incomeCompletionStatus, expensesCompletionStatus, nationalInsuranceCompletionStatus) =
+      (getStatus(Abroad), getStatus(Income), getStatus(Expenses), getStatus(NationalInsurance))
 
     val (abroadUrlString, incomeUrlString, expensesUrlString, nationalInsuranceUrlString) =
-      (SelfEmploymentAbroadController.onPageLoad(taxYear, business.businessId, if (abroadStatus.isEmpty) NormalMode else CheckMode).url,
+      (SelfEmploymentAbroadController.onPageLoad(taxYear, business.businessId, if (abroadCompletionStatus.isEmpty) NormalMode else CheckMode).url,
+        if (abroadCompletionStatus.getOrElse(false)) "#" else "#", //TODO replace first # with income journey url when created
         "#",
+        "#")
+
+    val (abroadStatusString, incomeStatusString, expensesStatusString, nationalInsuranceStatusString) =
+      (
+        if (abroadCompletionStatus.isEmpty) notStartedStatus
+        else if (abroadCompletionStatus.get) completedStatus
+        else inProgressStatus,
+
+        if (abroadCompletionStatus.getOrElse(false)) cannotStartYetStatus
+        else if (incomeCompletionStatus.isEmpty) notStartedStatus
+        else if (incomeCompletionStatus.get) completedStatus
+        else inProgressStatus,
+
         "#",
+
         "#")
 
     SummaryList(
       rows = Seq(
-        buildRow("selfEmploymentAbroad", abroadUrlString, statusToString(abroadStatus)),
-        buildRow("income", incomeUrlString, statusToString(incomeStatus, abroadStatus)),
-        buildRow("expensesCategories", expensesUrlString, statusToString(expensesStatus, incomeStatus)),
-        buildRow("nationalInsurance", nationalInsuranceUrlString, statusToString(nationalInsuranceStatus, expensesStatus))
+        buildRow("selfEmploymentAbroad", abroadUrlString, abroadStatusString),
+        buildRow("income", incomeUrlString, incomeStatusString),
+        buildRow("expensesCategories", expensesUrlString, expensesStatusString),
+        buildRow("nationalInsurance", nationalInsuranceUrlString, nationalInsuranceStatusString)
       ),
       classes = "govuk-!-margin-bottom-7")
   }
@@ -74,14 +89,6 @@ object TradeJourneyStatusesViewModel {
         href = href,
         content = HtmlContent(s"<strong class='govuk-tag app-task-list__tag govuk-tag--$status'> $statusString </strong>")))
     ).withCssClass("app-task-list__item")
-  }
-
-  private def statusToString(status: Option[Boolean], prevStatus: Option[Boolean] = Some(false)): String = {
-    status match {
-      case Some(true) => completedStatus
-      case Some(false) => inProgressStatus
-      case _ => if (prevStatus.isEmpty) cannotStartYetStatus else notStartedStatus
-    }
   }
 
 }
