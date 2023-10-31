960ef6c4ba62bd9fe866d581bf95aa151d200881
Harry Ritchie <111759395+harry-ritchie@users.noreply.github.com>
2023-10-31 10:40:03 +0000

    SASS-6007: Previous commit didn't work

--- app/models/journeys/income/IncomeJourneyAnswers.scala
+++ app/models/journeys/income/IncomeJourneyAnswers.scala
@@ -16,6 +16,8 @@
 
 package models.journeys.income
 
+import models.HowMuchTradingAllowance.{LessThan, Maximum}
+import models.TradingAllowance.{DeclareExpenses, UseTradingAllowance}
 import models.{HowMuchTradingAllowance, TradingAllowance}
 import play.api.libs.json.{Json, OFormat}
 
@@ -28,7 +30,60 @@ case class IncomeJourneyAnswers(incomeNotCountedAsTurnover: Boolean,
                                 notTaxableAmount: Option[BigDecimal],
                                 tradingAllowance: TradingAllowance,
                                 howMuchTradingAllowance: Option[HowMuchTradingAllowance],
-                                tradingAllowanceAmount: Option[BigDecimal])
+                                tradingAllowanceAmount: Option[BigDecimal]) {
+
+  require(
+    incomeNotCountedAsTurnoverValidation(this) &&
+      anyOtherIncomeValidation(this) &&
+      turnoverNotTaxableValidation(this) &&
+      tradingAllowanceValidation(this) &&
+      howMuchTradingAllowanceValidation(this),
+    "provided case class parameters does not follow the allowed flow of pages"
+  )
+  /*
+   * TODO We may want to factor in the affect of cash/accrual as an additional constraint as to what params are allowed,
+   *  however this is not done here because the question of whether or not we want to add a cash/accrual flag to this
+   *  case case class (or apply the constraint elsewhere) should consider if this would unnecessarily perturb the model
+   *  for sending to our backend. If our back-end also needs this flag, consider putting it in here.
+   */
+
+  private def incomeNotCountedAsTurnoverValidation(incomeJourneyAnswers: IncomeJourneyAnswers): Boolean =
+    if (incomeJourneyAnswers.incomeNotCountedAsTurnover) {
+      incomeJourneyAnswers.nonTurnoverIncomeAmount.isDefined
+    } else {
+      incomeJourneyAnswers.nonTurnoverIncomeAmount.isEmpty
+    }
+
+  private def anyOtherIncomeValidation(incomeJourneyAnswers: IncomeJourneyAnswers): Boolean =
+    if (incomeJourneyAnswers.anyOtherIncome) {
+      incomeJourneyAnswers.otherIncomeAmount.isDefined
+    } else {
+      incomeJourneyAnswers.nonTurnoverIncomeAmount.isEmpty
+    }
+
+  private def turnoverNotTaxableValidation(incomeJourneyAnswers: IncomeJourneyAnswers): Boolean =
+    incomeJourneyAnswers.turnoverNotTaxable match {
+      case Some(isNotTaxable) => if (isNotTaxable) incomeJourneyAnswers.notTaxableAmount.isDefined else incomeJourneyAnswers.notTaxableAmount.isEmpty
+      case None               => incomeJourneyAnswers.notTaxableAmount.isEmpty
+    }
+
+  private def tradingAllowanceValidation(incomeJourneyAnswers: IncomeJourneyAnswers): Boolean =
+    incomeJourneyAnswers.tradingAllowance match {
+      case UseTradingAllowance =>
+        incomeJourneyAnswers.howMuchTradingAllowance.isDefined
+
+      case DeclareExpenses =>
+        incomeJourneyAnswers.howMuchTradingAllowance.isEmpty && incomeJourneyAnswers.tradingAllowanceAmount.isEmpty
+
+    }
+
+  private def howMuchTradingAllowanceValidation(incomeJourneyAnswers: IncomeJourneyAnswers): Boolean =
+    incomeJourneyAnswers.howMuchTradingAllowance match {
+      case i if i.contains(Maximum) || i.isEmpty => incomeJourneyAnswers.tradingAllowanceAmount.isEmpty
+      case Some(LessThan)                        => incomeJourneyAnswers.tradingAllowanceAmount.isDefined
+    }
+
+}
 
 object IncomeJourneyAnswers {
   implicit val formats: OFormat[IncomeJourneyAnswers] = Json.format[IncomeJourneyAnswers]
--- test/models/journeys/income/IncomeJourneyAnswersSpec.scala
+++ test/models/journeys/income/IncomeJourneyAnswersSpec.scala
@@ -1,3 +1,139 @@
-package models.journeys.income class IncomeJourneyAnswersSpec {
+package models.journeys.income
+
+import models.{HowMuchTradingAllowance, TradingAllowance}
+import org.scalatest.matchers.should.Matchers
+import org.scalatest.wordspec.AnyWordSpec
+
+class IncomeJourneyAnswersSpec extends AnyWordSpec with Matchers {
+
+  private val allowedIncomeJourneyAnswers = IncomeJourneyAnswers(
+    incomeNotCountedAsTurnover = true,
+    nonTurnoverIncomeAmount = Some(123.45),
+    turnoverIncomeAmount = 123.45,
+    anyOtherIncome = true,
+    otherIncomeAmount = Some(123.45),
+    turnoverNotTaxable = Some(true),
+    notTaxableAmount = Some(123.45),
+    tradingAllowance = TradingAllowance.UseTradingAllowance,
+    howMuchTradingAllowance = Some(HowMuchTradingAllowance.LessThan),
+    tradingAllowanceAmount = Some(123.45)
+  )
+
+  "IncomeJourneyAnswers" when {
+    "answering `No` to the IncomeNotCountedAsTurnover question" must {
+      "forbid the presence of an amount" in {
+        lazy val answers = allowedIncomeJourneyAnswers.copy(incomeNotCountedAsTurnover = false)
+
+        val result = intercept[IllegalArgumentException](answers)
+
+        result shouldBe a[IllegalArgumentException]
+      }
+    }
+    "answering `Yes` to the IncomeNotCountedAsTurnover question" must {
+      "enforce an amount" in {
+        lazy val answers = allowedIncomeJourneyAnswers.copy(nonTurnoverIncomeAmount = None)
+
+        val result = intercept[IllegalArgumentException](answers)
+
+        result shouldBe a[IllegalArgumentException]
+      }
+
+    }
+    "answering `No` to the AnyOtherIncome question" must {
+      "forbid the presence of an amount" in {
+        lazy val answers = allowedIncomeJourneyAnswers.copy(anyOtherIncome = false)
+
+        val result = intercept[IllegalArgumentException](answers)
+
+        result shouldBe a[IllegalArgumentException]
+      }
+    }
+    "answering `Yes` to the AnyOtherIncome question" must {
+      "enforce of an amount" in {
+        lazy val answers = allowedIncomeJourneyAnswers.copy(otherIncomeAmount = None)
+
+        val result = intercept[IllegalArgumentException](answers)
+
+        result shouldBe a[IllegalArgumentException]
+      }
+
+    }
+
+    "answering `No` to the TurnoverNotTaxable question" must {
+      "forbid the presence of an amount" in {
+        lazy val answers = allowedIncomeJourneyAnswers.copy(turnoverNotTaxable = Some(false))
+
+        val result = intercept[IllegalArgumentException](answers)
+
+        result shouldBe a[IllegalArgumentException]
+      }
+    }
+    "answering `Yes` to the TurnoverNotTaxable question" must {
+      "enforce an amount" in {
+        lazy val answers = allowedIncomeJourneyAnswers.copy(notTaxableAmount = None)
+
+        val result = intercept[IllegalArgumentException](answers)
+
+        result shouldBe a[IllegalArgumentException]
+      }
+
+    }
+    "not answering the TurnoverNotTaxable question" must {
+      "enforce an amount" in {
+        lazy val answers = allowedIncomeJourneyAnswers.copy(turnoverNotTaxable = None)
+
+        val result = intercept[IllegalArgumentException](answers)
+
+        result shouldBe a[IllegalArgumentException]
+      }
+    }
+
+    "choosing to declare expenses when prompted the TradingAllowance question" must {
+      "forbid the presence of an answer to how much of your trading allowance question" in {
+        lazy val answers = allowedIncomeJourneyAnswers.copy(tradingAllowance = TradingAllowance.DeclareExpenses)
+
+        val result = intercept[IllegalArgumentException](answers)
+
+        result shouldBe a[IllegalArgumentException]
+      }
+    }
+    "choosing to use the trading allowance when prompted the TradingAllowance question" must {
+      "enforce an answer to the how much of your trading allowance question" in {
+        lazy val answers = allowedIncomeJourneyAnswers.copy(howMuchTradingAllowance = None)
+
+        val result = intercept[IllegalArgumentException](answers)
+
+        result shouldBe a[IllegalArgumentException]
+      }
+
+    }
+    "choosing to use the maximum trading allowance when prompted the HowMuchTradingAllowance question" must {
+      "forbid the presence of an answer to the follow up amount question" in {
+        lazy val answers = allowedIncomeJourneyAnswers.copy(howMuchTradingAllowance = Some(HowMuchTradingAllowance.Maximum))
+
+        val result = intercept[IllegalArgumentException](answers)
+
+        result shouldBe a[IllegalArgumentException]
+      }
+    }
+    "not answering the HowMuchTradingAllowance question" must {
+      "forbid the presence of an answer to the follow up amount question" in {
+        lazy val answers = allowedIncomeJourneyAnswers.copy(howMuchTradingAllowance = None)
+
+        val result = intercept[IllegalArgumentException](answers)
+
+        result shouldBe a[IllegalArgumentException]
+      }
+    }
+    "choosing to use less than the maximum trading allowance when prompted the HowMuchTradingAllowance question" must {
+      "enforce an answer to the follow up amount question" in {
+        lazy val answers = allowedIncomeJourneyAnswers.copy(tradingAllowanceAmount = None)
+
+        val result = intercept[IllegalArgumentException](answers)
+
+        result shouldBe a[IllegalArgumentException]
+      }
+    }
+  }
 
 }
