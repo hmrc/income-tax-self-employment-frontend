64b9d628f16f9b12ae97fc2c53499a70ba93f86a
Jacob Raffe <jakeraffe24@gmail.com>
2023-12-13 16:38:35 +0000

    [SASS-6494] Merge conflict

--- app/models/requests/DataRequest.scala
+++ app/models/requests/DataRequest.scala
@@ -35,7 +35,6 @@ case class OptionalDataRequest[A](request: Request[A], userId: String, user: Use
 
 case class DataRequest[A](request: Request[A], userId: String, user: User, userAnswers: UserAnswers) extends WrappedRequest[A](request) {
   val userType: UserType = user.userType
-  val nino: Nino         = Nino(user.nino)
   val mtditid: Mtditid   = Mtditid(user.mtditid)
   val nino: Nino         = Nino(user.nino)
 
