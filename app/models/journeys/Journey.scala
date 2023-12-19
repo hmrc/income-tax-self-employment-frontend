ba6dae905ccc73d82d96b46f34c91c70e54745a5
Harry Ritchie <111759395+harry-ritchie@users.noreply.github.com>
2023-12-19 14:22:41 +0000

    SASS-6572: Load prior data for office supplies expenses journey

--- app/models/journeys/Journey.scala
+++ app/models/journeys/Journey.scala
@@ -18,6 +18,7 @@ package models.journeys
 
 import models.common.PageName
 import pages.expenses.goodsToSellOrUse.{DisallowableGoodsToSellOrUseAmountPage, GoodsToSellOrUseAmountPage}
+import pages.expenses.officeSupplies.{OfficeSuppliesAmountPage, OfficeSuppliesDisallowableAmountPage}
 import pages.income._
 import play.api.libs.json._
 
@@ -68,6 +69,8 @@ object Journey {
 
   case object ExpensesOfficeSupplies extends Journey {
     override def toString: String = "expenses-office-supplies"
+
+    override val pageKeys: List[PageName] = List(OfficeSuppliesAmountPage.pageName, OfficeSuppliesDisallowableAmountPage.pageName)
   }
 
   case object ExpensesEntertainment extends Journey {
