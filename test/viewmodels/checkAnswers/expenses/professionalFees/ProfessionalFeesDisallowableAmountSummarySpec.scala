12565aa0c9830d8c669bb66cdb5489ad90f98e0b
Jacob Raffe <jakeraffe24@gmail.com>
2023-12-27 16:50:56 +0000

    [SASS-6606] ProfessionalFees - Create amount and disallowable amount pages

---
+++ test/viewmodels/checkAnswers/expenses/professionalFees/ProfessionalFeesDisallowableAmountSummarySpec.scala
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package viewmodels.checkAnswers.expenses.professionalFees
+
+import base.summaries.SummaryBaseSpec
+import models.common.UserType
+import models.database.UserAnswers
+import play.api.libs.json.{JsObject, Json}
+import uk.gov.hmrc.govukfrontend.views.viewmodels.content.Text
+import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryListRow
+import utils.MoneyUtils.formatMoney
+
+class ProfessionalFeesDisallowableAmountSummarySpec extends SummaryBaseSpec("ProfessionalFeesDisallowableAmountSummary") {
+
+  private lazy val allowableAmount: BigDecimal    = 500
+  private lazy val disallowableAmount: BigDecimal = 20
+
+  override lazy val validData: JsObject = Json.obj(
+    "disallowableProfessionalFees"       -> "yes",
+    "professionalFeesAmount"             -> allowableAmount,
+    "professionalFeesDisallowableAmount" -> disallowableAmount
+  )
+  override lazy val invalidData: JsObject = Json.obj("otherPage" -> disallowableAmount)
+
+  override lazy val testKey: UserType => Text = (userType: UserType) =>
+    Text(messages(s"professionalFeesDisallowableAmount.title.$userType", allowableAmount))
+  override lazy val testValue: Text = Text(s"£${formatMoney(disallowableAmount)}")
+
+  override def buildSummaryListRow(userAnswers: UserAnswers, userType: UserType): Option[SummaryListRow] =
+    ProfessionalFeesDisallowableAmountSummary.row(userAnswers, taxYear, businessId, userType)(messages)
+
+}
