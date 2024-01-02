332bf586bcdefff2ce41834fcffafa456cc0bef7
harry-ritchie <111759395+harry-ritchie@users.noreply.github.com>
2024-01-02 13:45:56 +0000

    SASS-6598: CYA Other Expenses (#176)
    * SASS-6598: CYA other expenses
    
    * SASS-6598: Add navigation and UTs
    
    * SASS-6598: Formatting
    
    * SASS-6598: Formatting
    
    * SASS-6598: Address some pr comments

---
+++ app/controllers/journeys/expenses/otherExpenses/OtherExpensesCYAController.scala
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.journeys.expenses.otherExpenses
+
+import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
+import controllers.journeys.expenses.otherExpenses.routes.OtherExpensesCYAController
+import models.common.{BusinessId, TaxYear}
+import pages.expenses.otherExpenses.OtherExpensesCYAPage
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import viewmodels.checkAnswers.expenses.otherExpenses.{OtherExpensesAmountSummary, OtherExpensesDisallowableAmountSummary}
+import viewmodels.journeys.SummaryListCYA.summaryListOpt
+import views.html.standard.CheckYourAnswersView
+
+import javax.inject.Inject
+
+class OtherExpensesCYAController @Inject() (override val messagesApi: MessagesApi,
+                                            val controllerComponents: MessagesControllerComponents,
+                                            identify: IdentifierAction,
+                                            getAnswers: DataRetrievalAction,
+                                            requireAnswers: DataRequiredAction,
+                                            view: CheckYourAnswersView)
+    extends FrontendBaseController
+    with I18nSupport {
+
+  def onPageLoad(taxYear: TaxYear, businessId: BusinessId): Action[AnyContent] = (identify andThen getAnswers andThen requireAnswers) {
+    implicit request =>
+      val user = request.userType
+
+      val summaryList = summaryListOpt(
+        List(
+          OtherExpensesAmountSummary.row(request.userAnswers, taxYear, businessId, user),
+          OtherExpensesDisallowableAmountSummary.row(request.userAnswers, taxYear, businessId, user)
+        ))
+
+      Ok(
+        view(OtherExpensesCYAPage.toString, taxYear, request.user.userType, summaryList, OtherExpensesCYAController.onPageLoad(taxYear, businessId))
+      )
+  }
+
+  // TODO Implement Save & Continue in SASS-6216
+  def onSubmit(taxYear: TaxYear, businessId: BusinessId): Action[AnyContent] = (identify andThen getAnswers andThen requireAnswers) { _ =>
+    Redirect(OtherExpensesCYAController.onPageLoad(taxYear, businessId))
+  }
+
+}
