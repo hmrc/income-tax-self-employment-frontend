90eb20b7f1bab14177b38f9690a956f284dabc12
Jacob Raffe <jakeraffe24@gmail.com>
2024-01-03 17:25:04 +0000

    [SASS-6271] Professional fees - Add to task list

--- test/viewmodels/TradeJourneyStatusesViewModelSpec.scala
+++ test/viewmodels/TradeJourneyStatusesViewModelSpec.scala
@@ -104,6 +104,10 @@ class TradeJourneyStatusesViewModelSpec extends SpecBase with TableDrivenPropert
       tailoringCyaIsAnswered && userAnswers.get(ProfessionalServiceExpensesPage, Some(businessId)).contains(ProfessionalServiceExpenses.Staff)
     val constructionIsYes =
       tailoringCyaIsAnswered && userAnswers.get(ProfessionalServiceExpensesPage, Some(businessId)).contains(ProfessionalServiceExpenses.Construction)
+    val professionalFeesIsYes =
+      tailoringCyaIsAnswered && userAnswers
+        .get(ProfessionalServiceExpensesPage, Some(businessId))
+        .contains(ProfessionalServiceExpenses.ProfessionalFees)
     val depreciationIsYes = tailoringCyaIsAnswered && userAnswers.get(DepreciationPage, Some(businessId)).exists(_ != Depreciation.No)
 
     Seq(
@@ -120,6 +124,7 @@ class TradeJourneyStatusesViewModelSpec extends SpecBase with TableDrivenPropert
       buildOptionalRow(ExpensesEntertainment, findJourneyStatus(journeyCompletedStates, ExpensesEntertainment), entertainmentsIsYes),
       buildOptionalRow(ExpensesStaffCosts, findJourneyStatus(journeyCompletedStates, ExpensesStaffCosts), staffCostsIsYes),
       buildOptionalRow(ExpensesConstruction, findJourneyStatus(journeyCompletedStates, ExpensesConstruction), constructionIsYes),
+      buildOptionalRow(ExpensesProfessionalFees, findJourneyStatus(journeyCompletedStates, ExpensesProfessionalFees), professionalFeesIsYes),
       buildOptionalRow(ExpensesDepreciation, findJourneyStatus(journeyCompletedStates, ExpensesDepreciation), depreciationIsYes)
     ).flatten
   }
@@ -170,6 +175,8 @@ class TradeJourneyStatusesViewModelSpec extends SpecBase with TableDrivenPropert
         journeys.expenses.staffCosts.routes.StaffCostsAmountController.onPageLoad(taxYear, businessId, NormalMode).url
       case ExpensesConstruction =>
         journeys.expenses.construction.routes.ConstructionIndustryAmountController.onPageLoad(taxYear, businessId, NormalMode).url
+      case ExpensesProfessionalFees =>
+        journeys.expenses.professionalFees.routes.ProfessionalFeesAmountController.onPageLoad(taxYear, businessId, NormalMode).url
       case ExpensesDepreciation =>
         journeys.expenses.depreciation.routes.DepreciationDisallowableAmountController.onPageLoad(taxYear, businessId, NormalMode).url
       case _ => "not implemented or error"
@@ -186,9 +193,10 @@ class TradeJourneyStatusesViewModelSpec extends SpecBase with TableDrivenPropert
         journeys.expenses.repairsandmaintenance.routes.RepairsAndMaintenanceCostsCYAController.onPageLoad(taxYear, businessId).url
       case ExpensesAdvertisingOrMarketing =>
         journeys.expenses.advertisingOrMarketing.routes.AdvertisingCYAController.onPageLoad(taxYear, businessId).url
-      case ExpensesEntertainment => journeys.expenses.entertainment.routes.EntertainmentCYAController.onPageLoad(taxYear, businessId).url
-      case ExpensesStaffCosts    => journeys.expenses.staffCosts.routes.StaffCostsCYAController.onPageLoad(taxYear, businessId).url
-      case ExpensesConstruction  => journeys.expenses.construction.routes.ConstructionIndustryCYAController.onPageLoad(taxYear, businessId).url
+      case ExpensesEntertainment    => journeys.expenses.entertainment.routes.EntertainmentCYAController.onPageLoad(taxYear, businessId).url
+      case ExpensesStaffCosts       => journeys.expenses.staffCosts.routes.StaffCostsCYAController.onPageLoad(taxYear, businessId).url
+      case ExpensesConstruction     => journeys.expenses.construction.routes.ConstructionIndustryCYAController.onPageLoad(taxYear, businessId).url
+      case ExpensesProfessionalFees => journeys.expenses.professionalFees.routes.ProfessionalFeesCYAController.onPageLoad(taxYear, businessId).url
       case ExpensesDepreciation =>
         journeys.expenses.depreciation.routes.DepreciationCYAController.onPageLoad(taxYear, businessId).url
       case _ => "not implemented or error"
