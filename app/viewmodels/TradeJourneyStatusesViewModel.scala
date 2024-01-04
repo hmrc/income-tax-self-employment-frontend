2cb712ae9f55069de0c3591632cd5c9042e4d5ed
Harry Ritchie <111759395+harry-ritchie@users.noreply.github.com>
2024-01-04 13:38:41 +0000

    SASS-6725: Add case

--- app/viewmodels/TradeJourneyStatusesViewModel.scala
+++ app/viewmodels/TradeJourneyStatusesViewModel.scala
@@ -248,7 +248,8 @@ object TradeJourneyStatusesViewModel {
             .url,
           expenses.depreciation.routes.DepreciationCYAController.onPageLoad(taxYear, businessId).url
         )
-      case ExpensesInterest | ExpensesTotal | NationalInsurance | TradeDetails | ExpensesAdvertisingOrMarketing | ExpensesOtherExpenses =>
+      case ExpensesInterest | ExpensesTotal | NationalInsurance | TradeDetails | ExpensesAdvertisingOrMarketing | ExpensesOtherExpenses |
+          ExpensesFinancialCharges =>
         ??? // TODO Other Journeys not yet implemented
     }
   }
