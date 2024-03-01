225273e00ef0ed9628bc836b6ce9c29c4b050f7f
Jacob Raffe <jakeraffe24@gmail.com>
2024-03-01 13:22:12 +0000

    [SASS-7469] EVCP - Save data & Load prior data (#279)
    * [SASS-7469] EVCP - Load prior data
    
    * [SASS-7469] EVCP - Save data & Load prior data

--- app/controllers/journeys/capitalallowances/electricVehicleChargePoints/ElectricVehicleChargePointsCYAController.scala
+++ app/controllers/journeys/capitalallowances/electricVehicleChargePoints/ElectricVehicleChargePointsCYAController.scala
@@ -16,15 +16,16 @@
 
 package controllers.journeys.capitalallowances.electricVehicleChargePoints
 
-import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
-import controllers.journeys
+import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction, SubmittedDataRetrievalActionProvider}
+import controllers.handleSubmitAnswersResult
 import controllers.journeys.capitalallowances.electricVehicleChargePoints
-import models.NormalMode
 import models.common._
 import models.journeys.Journey.CapitalAllowancesElectricVehicleChargePoints
+import models.journeys.capitalallowances.electricVehicleChargePoints.ElectricVehicleChargePointsAnswers
 import pages.capitalallowances.tailoring.CapitalAllowancesCYAPage
 import play.api.i18n.{I18nSupport, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import services.SelfEmploymentService
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import utils.Logging
 import viewmodels.checkAnswers.capitalallowances.electricVehicleChargePoints._
@@ -32,20 +33,24 @@ import viewmodels.journeys.SummaryListCYA
 import views.html.standard.CheckYourAnswersView
 
 import javax.inject.{Inject, Singleton}
+import scala.concurrent.ExecutionContext
 
 @Singleton
 class ElectricVehicleChargePointsCYAController @Inject() (override val messagesApi: MessagesApi,
                                                           identify: IdentifierAction,
                                                           getAnswers: DataRetrievalAction,
+                                                          getJourneyAnswers: SubmittedDataRetrievalActionProvider,
                                                           requireAnswers: DataRequiredAction,
+                                                          service: SelfEmploymentService,
                                                           val controllerComponents: MessagesControllerComponents,
-                                                          view: CheckYourAnswersView)
+                                                          view: CheckYourAnswersView)(implicit ec: ExecutionContext)
     extends FrontendBaseController
     with I18nSupport
     with Logging {
 
   def onPageLoad(taxYear: TaxYear, businessId: BusinessId): Action[AnyContent] =
-    (identify andThen getAnswers andThen requireAnswers) { implicit request =>
+    (identify andThen getAnswers andThen getJourneyAnswers[ElectricVehicleChargePointsAnswers](req =>
+      req.mkJourneyNinoContext(taxYear, businessId, CapitalAllowancesElectricVehicleChargePoints)) andThen requireAnswers) { implicit request =>
       val summaryList =
         SummaryListCYA.summaryListOpt(
           List(
@@ -67,10 +72,11 @@ class ElectricVehicleChargePointsCYAController @Inject() (override val messagesA
         ))
     }
 
-  def onSubmit(taxYear: TaxYear, businessId: BusinessId): Action[AnyContent] = (identify andThen getAnswers andThen requireAnswers) { _ =>
-    Redirect(
-      journeys.routes.SectionCompletedStateController
-        .onPageLoad(taxYear, businessId, CapitalAllowancesElectricVehicleChargePoints.entryName, NormalMode))
+  def onSubmit(taxYear: TaxYear, businessId: BusinessId): Action[AnyContent] = (identify andThen getAnswers andThen requireAnswers) async {
+    implicit request =>
+      val context = JourneyContextWithNino(taxYear, request.nino, businessId, request.mtditid, CapitalAllowancesElectricVehicleChargePoints)
+      val result  = service.submitAnswers[ElectricVehicleChargePointsAnswers](context, request.userAnswers)
+      handleSubmitAnswersResult(context, result)
   }
 
 }
