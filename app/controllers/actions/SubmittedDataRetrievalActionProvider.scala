f72b9c3dd8177c80c584e005061801434380f2e2
Connie Bernardin <conniebernardin@googlemail.com>
2024-03-07 13:22:37 +0000

    fmt

--- app/controllers/actions/SubmittedDataRetrievalActionProvider.scala
+++ app/controllers/actions/SubmittedDataRetrievalActionProvider.scala
@@ -78,7 +78,11 @@ class SubmittedDataRetrievalActionProviderImpl @Inject() (connector: SelfEmploym
         gtsouUpdated,
         Journey.CapitalAllowancesTailoring)
       zecUpdated <- loadAnswers[ZeroEmissionCarsAnswers](taxYear, businesses, capitalAllowancesUpdated, Journey.CapitalAllowancesZeroEmissionCars)
-      balancingAllowanceUpdated <- loadAnswers[BalancingAllowanceAnswers](taxYear, businesses, zecUpdated, Journey.CapitalAllowancesBalancingAllowance)
+      balancingAllowanceUpdated <- loadAnswers[BalancingAllowanceAnswers](
+        taxYear,
+        businesses,
+        zecUpdated,
+        Journey.CapitalAllowancesBalancingAllowance)
       evcpUpdated <- loadAnswers[ElectricVehicleChargePointsAnswers](
         taxYear,
         businesses,
