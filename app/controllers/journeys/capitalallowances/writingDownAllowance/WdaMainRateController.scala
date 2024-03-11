cd248446befd6b13a428bcb511f6f1a223927d96
Lukasz Tymoszczuk <lukasz.tymoszczuk@digital.hmrc.gov.uk>
2024-03-11 15:38:25 +0000

    Writing down allowance pages

---
+++ app/controllers/journeys/capitalallowances/writingDownAllowance/WdaMainRateController.scala
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.journeys.capitalallowances.writingDownAllowance
+
+import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
+import forms.capitalallowances.writingDownAllowance.WritingDownAllowanceFormProvider
+import models.Mode
+import models.common.{BusinessId, TaxYear}
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import utils.Logging
+import views.html.journeys.capitalallowances.writingDownAllowance.WdaMainRateView
+
+import javax.inject.{Inject, Singleton}
+
+@Singleton
+class WdaMainRateController @Inject() (override val messagesApi: MessagesApi,
+                                       val controllerComponents: MessagesControllerComponents,
+                                       identify: IdentifierAction,
+                                       getData: DataRetrievalAction,
+                                       requireData: DataRequiredAction,
+                                       view: WdaMainRateView)
+    extends FrontendBaseController
+    with I18nSupport
+    with Logging {
+
+  def onPageLoad(taxYear: TaxYear, businessId: BusinessId, mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) {
+    implicit request =>
+      val form = WritingDownAllowanceFormProvider.apply()
+      Ok(view(form, mode, request.userType, taxYear, businessId))
+  }
+
+  def onSubmit(taxYear: TaxYear, businessId: BusinessId, mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) {
+    implicit request =>
+      val form = WritingDownAllowanceFormProvider.apply()
+      Ok(view(form, mode, request.userType, taxYear, businessId))
+  }
+}
---
+++ app/controllers/journeys/capitalallowances/writingDownAllowance/WritingDownAllowanceControllerCYAController.scala
@@ -0,0 +1,62 @@
+/*
+ * Copyright 2024 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.journeys.capitalallowances.writingDownAllowance
+
+import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
+import forms.capitalallowances.writingDownAllowance.WritingDownAllowanceFormProvider
+import models.Mode
+import models.common.{BusinessId, TaxYear}
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import utils.Logging
+import viewmodels.journeys.SummaryListCYA
+import views.html.standard.CheckYourAnswersView
+
+import javax.inject.{Inject, Singleton}
+
+@Singleton
+class WritingDownAllowanceControllerCYAController @Inject() (override val messagesApi: MessagesApi,
+                                                             val controllerComponents: MessagesControllerComponents,
+                                                             identify: IdentifierAction,
+                                                             getData: DataRetrievalAction,
+                                                             requireData: DataRequiredAction,
+                                                             view: CheckYourAnswersView)
+    extends FrontendBaseController
+    with I18nSupport
+    with Logging {
+  def onPageLoad(taxYear: TaxYear, businessId: BusinessId): Action[AnyContent] = (identify andThen getData andThen requireData) { implicit request =>
+    val summaryList =
+      SummaryListCYA(request.userAnswers, taxYear, businessId, request.userType).mkSummaryList(
+        List()
+      )
+
+    Ok(
+      view(
+        "test",
+        taxYear,
+        request.userType,
+        summaryList,
+        routes.WritingDownAllowanceControllerCYAController.onSubmit(taxYear, businessId)
+      ))
+  }
+
+  def onSubmit(taxYear: TaxYear, businessId: BusinessId): Action[AnyContent] = (identify andThen getData andThen requireData) async {
+    implicit request =>
+      ???
+  }
+}
