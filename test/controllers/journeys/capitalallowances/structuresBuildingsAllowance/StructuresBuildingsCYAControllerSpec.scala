1b7914c589e3e418dfe0f21666fe963fdcea0f38
Connie Bernardin <97225150+conniebernardin@users.noreply.github.com>
2024-03-14 15:07:25 +0000

    SASS 7621 : Create Are you eligible to claim Structures and buildings allowance page (#305)
    * create structures buildings eligible claim page
    
    * add to cya
    
    * add links

--- test/controllers/journeys/capitalallowances/structuresBuildingsAllowance/StructuresBuildingsCYAControllerSpec.scala
+++ test/controllers/journeys/capitalallowances/structuresBuildingsAllowance/StructuresBuildingsCYAControllerSpec.scala
@@ -24,7 +24,7 @@ import play.api.i18n.Messages
 import play.api.libs.json.{JsObject, Json}
 import play.api.mvc.Call
 import uk.gov.hmrc.govukfrontend.views.viewmodels.summarylist.SummaryList
-import viewmodels.checkAnswers.capitalallowances.structuresBuildingsAllowance._
+import viewmodels.checkAnswers.capitalallowances.structuresBuildingsAllowance.{StructuresBuildingsEligibleClaimSummary, _}
 
 class StructuresBuildingsCYAControllerSpec extends CYAOnPageLoadControllerBaseSpec {
 
@@ -35,7 +35,8 @@ class StructuresBuildingsCYAControllerSpec extends CYAOnPageLoadControllerBaseSp
       "structuresBuildingsAllowance"             -> false,
       "structuresBuildingsClaimed"               -> false,
       "structuresBuildingsPreviousClaimUse"      -> false,
-      "structuresBuildingsPreviousClaimedAmount" -> 12
+      "structuresBuildingsPreviousClaimedAmount" -> 12,
+      "structuresBuildingsEligibleClaim"         -> false
     ))
 
   def onPageLoadCall: (TaxYear, BusinessId) => Call = routes.StructuresBuildingsCYAController.onPageLoad
@@ -48,7 +49,8 @@ class StructuresBuildingsCYAControllerSpec extends CYAOnPageLoadControllerBaseSp
         StructuresBuildingsAllowanceSummary.row(userAnswers, taxYear, businessId, userType).value,
         StructuresBuildingsClaimedSummary.row(userAnswers, taxYear, businessId, userType).value,
         StructuresBuildingsPreviousClaimUseSummary.row(userAnswers, taxYear, businessId, userType).value,
-        StructuresBuildingsPreviousClaimedAmountSummary.row(userAnswers, taxYear, businessId, userType).value
+        StructuresBuildingsPreviousClaimedAmountSummary.row(userAnswers, taxYear, businessId, userType).value,
+        StructuresBuildingsEligibleClaimSummary.row(userAnswers, taxYear, businessId, userType).value
       ),
       classes = "govuk-!-margin-bottom-7"
     )
