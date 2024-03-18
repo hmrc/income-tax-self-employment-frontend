17f2c6d3f468190874174ccaa5cc4684e802ff23
Lukasz Tymoszczuk <lukasz.tymoszczuk@digital.hmrc.gov.uk>
2024-03-18 22:07:52 +0000

    clean up

--- test/pages/PageSpecBase.scala
+++ test/pages/PageSpecBase.scala
@@ -20,7 +20,7 @@ import base.SpecBase
 import base.SpecBase.buildUserAnswers
 import org.scalatest.prop.{TableDrivenPropertyChecks, TableFor2}
 import org.scalatest.wordspec.AnyWordSpecLike
-import play.api.libs.json.{JsObject, Json}
+import play.api.libs.json.JsObject
 
 abstract class PageSpecBase(page: OneQuestionPage[_]) extends TableDrivenPropertyChecks with AnyWordSpecLike {
   val hasAllFurtherAnswersCases: TableFor2[JsObject, Boolean]
