528cb9c019e0cf99fdb96c27d492fa4e234ea7be
“Kuldeep <kuldeepwadhwa9@gmail.com>
2024-08-21 18:30:40 +0100

    SASS-8636 P&L:Adjusted taxable profit-create content page

--- app/controllers/journeys/adjustments/profitOrLoss/ProfitOrLossCalculationController.scala
+++ app/controllers/journeys/adjustments/profitOrLoss/ProfitOrLossCalculationController.scala
@@ -17,14 +17,17 @@
 package controllers.journeys.adjustments.profitOrLoss
 
 import controllers.actions.{DataRequiredAction, DataRetrievalAction, IdentifierAction}
-import models.NormalMode
 import controllers.journeys
+import models.NormalMode
 import models.common._
 import models.journeys.Journey.ProfitOrLoss
-import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.i18n.{I18nSupport, Messages, MessagesApi}
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.govukfrontend.views.viewmodels.table.Table
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
 import utils.Logging
+import utils.MoneyUtils.formatSumMoneyNoNegative
+import viewmodels.checkAnswers.{buildTable, buildTableAmountRow}
 import viewmodels.journeys.SummaryListCYA
 import views.html.journeys.adjustments.profitOrLoss.ProfitOrLossCalculationView
 
@@ -42,12 +45,96 @@ class ProfitOrLossCalculationController @Inject() (override val messagesApi: Mes
     with Logging {
 
   def onPageLoad(taxYear: TaxYear, businessId: BusinessId): Action[AnyContent] = (identify andThen getData andThen requireData) { implicit request =>
-    val summaryList = SummaryListCYA.summaryListOpt(List())
+    val summaryList        = SummaryListCYA.summaryListOpt(List())
+    val netAmount          = BigDecimal(4600)
+    val formattedNetAmount = formatSumMoneyNoNegative(List(netAmount))
+    val table1             = buildTable1(taxYear)
+    val table2             = buildTable2()
+    val table3             = buildTable3()
+    val table4             = buildTable4()
+    val table5             = buildTable5()
     Ok(
       view(
         request.userType,
+        formattedNetAmount,
+        taxYear,
         summaryList,
-        journeys.routes.SectionCompletedStateController.onPageLoad(taxYear, businessId, ProfitOrLoss.entryName, NormalMode)))
+        table1,
+        table2,
+        table3,
+        table4,
+        table5,
+        journeys.routes.SectionCompletedStateController.onPageLoad(taxYear, businessId, ProfitOrLoss.entryName, NormalMode)
+      ))
+  }
+
+  private def buildTable1(taxYear: TaxYear)(implicit messages: Messages): Table = {
+
+    val startYear = taxYear.startYear.toString
+    val endYear   = taxYear.endYear.toString
+
+    val adjustableTaxableProfitRows = Seq(
+      buildTableAmountRow("profitOrLoss.netProfitOrLoss.profit", 4400.00),
+      buildTableAmountRow("profitOrLoss.additions.profit", 200.00),
+      buildTableAmountRow("profitOrLoss.deductions.profit", 0.00),
+      buildTableAmountRow("profitOrLoss.netForTaxPurposes.profit", 4600.00),
+      buildTableAmountRow("journeys.adjustments", 0.00),
+      buildTableAmountRow(
+        "profitOrLossCalculation.adjustedTable.profit",
+        4600.00,
+        classes = "govuk-!-font-weight-bold",
+        optArgs = Seq(startYear, endYear))
+    )
+    buildTable(None, adjustableTaxableProfitRows)
+  }
+
+  private def buildTable2()(implicit messages: Messages): Table = {
+
+    val netProfit = Seq(
+      buildTableAmountRow("profitOrLoss.turnover", 5000.00),
+      buildTableAmountRow("incomeNotCountedAsTurnover.title", 0.00),
+      buildTableAmountRow("profitOrLoss.totalExpenses", -600.00),
+      buildTableAmountRow("profitOrLoss.netProfitOrLoss.profit", 4400.25)
+    )
+    buildTable(None, netProfit, caption = Some(messages(s"profitOrLoss.netProfitOrLoss.profit")), "govuk-!-margin-top-6 govuk-!-margin-bottom-9")
+  }
+
+  private def buildTable3()(implicit messages: Messages): Table = {
+
+    val additionsToNetProfitTable = Seq(
+      buildTableAmountRow("selectCapitalAllowances.balancingCharge", 0.00),
+      buildTableAmountRow("goodsAndServicesForYourOwnUse.title.individual", 200.00),
+      buildTableAmountRow("profitOrLoss.disallowableExpenses", 0.00),
+      buildTableAmountRow("profitOrLoss.totalAdditions.profit", 200.00)
+    )
+    buildTable(
+      None,
+      additionsToNetProfitTable,
+      caption = Some(messages(s"profitOrLoss.additions.profit")),
+      "govuk-!-margin-top-6 govuk-!-margin-bottom-9")
+  }
+
+  private def buildTable4()(implicit messages: Messages): Table = {
+
+    val capitalAllowanceTable = Seq(
+      buildTableAmountRow("profitOrLoss.capitalAllowances", 0.00),
+      buildTableAmountRow("profitOrLoss.turnoverNotTaxable", 0.00),
+      buildTableAmountRow("profitOrLoss.totalDeductions.profit", 0.00)
+    )
+    buildTable(
+      None,
+      capitalAllowanceTable,
+      caption = Some(messages(s"profitOrLoss.deductions.profit")),
+      "govuk-!-margin-top-6 govuk-!-margin-bottom-9")
+  }
+
+  private def buildTable5()(implicit messages: Messages): Table = {
+
+    val adjustmentsTable = Seq(
+      buildTableAmountRow("adjustments.anyOtherBusinessIncome", 0.00),
+      buildTableAmountRow("adjustments.totalAdjustments", 0.00)
+    )
+    buildTable(None, adjustmentsTable, caption = Some(messages("journeys.adjustments")), "govuk-!-margin-top-6 govuk-!-margin-bottom-9")
   }
 
 }
