4742ee8759ec41e9894c3c840ffdbdb6f8cc2a6e
ashwinirudrappa <11343826+ashwinirudrappa@users.noreply.github.com>
2025-01-17 11:11:43 +0000

    Format the file

--- test/models/journeys/nics/TaxableProfitAndLossSpec.scala
+++ test/models/journeys/nics/TaxableProfitAndLossSpec.scala
@@ -52,7 +52,6 @@ class TaxableProfitAndLossSpec extends AnyFreeSpec with ScalaCheckPropertyChecks
   "returnClassTwoOrFourEligible" - {
     "must return the correct class object depending on if the user is Class 2 or 4 eligible or neither" ignore {
       forAll(testScenarios) { case (taxableProfitsAndLosses, dateOfBirth, expectedResult) =>
-
         val result = TaxableProfitAndLoss.returnClassTwoOrFourEligible(taxableProfitsAndLosses, dateOfBirth, taxYear)
 
         assert(result === expectedResult)
