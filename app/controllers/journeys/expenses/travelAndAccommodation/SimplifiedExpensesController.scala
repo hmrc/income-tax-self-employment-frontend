128a68188507054f4ca4f3a57d9c5d34e617a5af
vfryer12 <72739659+vfryer12@users.noreply.github.com>
2025-03-17 14:36:55 +0000

    [SASS-11336]Created outline for simplified expenses page

---
+++ app/controllers/journeys/expenses/travelAndAccommodation/SimplifiedExpensesController.scala
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.journeys.expenses.travelAndAccommodation
+
+import controllers.actions._
+import forms.expenses.travelAndAccommodation.SimplifiedExpenseFormProvider
+import models.Mode
+import models.common.{BusinessId, TaxYear}
+import pages.expenses.travelAndAccommodation.SimplifiedExpensesPage
+import play.api.data.Form
+import play.api.i18n.{I18nSupport, MessagesApi}
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import repositories.SessionRepository
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendBaseController
+import views.html.journeys.expenses.travelAndAccommodation.SimplifiedExpenseView
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class SimplifiedExpensesController @Inject() (
+    override val messagesApi: MessagesApi,
+    sessionRepository: SessionRepository,
+    navigator: Navigator,
+    identify: IdentifierAction,
+    getData: DataRetrievalAction,
+    requireData: DataRequiredAction,
+    formProvider: SimplifiedExpenseFormProvider,
+    val controllerComponents: MessagesControllerComponents,
+    view: SimplifiedExpenseView
+)(implicit ec: ExecutionContext)
+    extends FrontendBaseController
+    with I18nSupport {
+
+  val form: Form[Boolean] = formProvider()
+
+  def onPageLoad(taxYear: TaxYear, businessId: BusinessId, mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) {
+    implicit request =>
+      val preparedForm = request.userAnswers.get(SimplifiedExpensesPage) match {
+        case None        => form
+        case Some(value) => form.fill(value)
+      }
+
+      Ok(view(preparedForm, taxYear, businessId, mode))
+  }
+
+  def onSubmit(taxYear: TaxYear, businessId: BusinessId, mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData).async {
+    implicit request =>
+      form
+        .bindFromRequest()
+        .fold(
+          formWithErrors => Future.successful(BadRequest(view(formWithErrors, taxYear, businessId, mode))),
+          value =>
+            for {
+              updatedAnswers <- Future.fromTry(request.userAnswers.set(SimplifiedExpensesPage, value))
+              _              <- sessionRepository.set(updatedAnswers)
+            } yield Redirect(navigator.nextPage(SimplifiedExpensesPage, mode, updatedAnswers))
+        )
+  }
+}
