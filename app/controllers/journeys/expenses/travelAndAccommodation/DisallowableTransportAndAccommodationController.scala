8018febf66901be47b9644ebf2c45b4fa53162ab
ashwinirudrappa <11343826+ashwinirudrappa@users.noreply.github.com>
2025-03-28 11:24:30 +0000

    SASS-11360:Scaffold the new page

--- app/controllers/journeys/expenses/travelAndAccommodation/DisallowableTransportAndAccommodationController.scala
+++ app/controllers/journeys/expenses/travelAndAccommodation/DisallowableTransportAndAccommodationController.scala
@@ -18,6 +18,7 @@ package controllers.journeys.expenses.travelAndAccommodation
 
 import controllers.actions._
 import controllers.journeys.fillForm
+import forms.expenses.travelAndAccommodation.DisallowableTransportAndAccommodationFormProvider
 import forms.standard.CurrencyFormProvider
 import models.Mode
 import models.common.{BusinessId, TaxYear, UserType}
@@ -47,11 +48,10 @@ class DisallowableTransportAndAccommodationController @Inject() (
     with I18nSupport {
 
   private val page = DisallowableTransportAndAccommodationPage
-  private val form = (userType: UserType) => formProvider(page, userType, prefix = Some("disallowableTransportAndAccommodation"))
+  private val form = (userType: UserType) => formProvider(page, userType)
 
   def onPageLoad(taxYear: TaxYear, businessId: BusinessId, mode: Mode): Action[AnyContent] = (identify andThen getData andThen requireData) {
     implicit request =>
-
       val filledForm = fillForm(page, businessId, form(request.userType))
       Ok(view(filledForm, mode, request.userType, taxYear, businessId))
   }
