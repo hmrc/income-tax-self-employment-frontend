8dd340d74d6c4f012c49d986723d74ecf99497a8
ashwinirudrappa <11343826+ashwinirudrappa@users.noreply.github.com>
2025-05-19 12:07:31 +0100

    SASS-11608:Optimise imports

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -19,8 +19,8 @@ object AppDependencies {
     "uk.gov.hmrc"        %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
-    "org.typelevel"      %% "cats-core"                             % "2.13.0",
-    "com.beachape"       %% "enumeratum"                            % "1.7.6",
+    "org.typelevel"      %% "cats-core"                             % "2.12.0",
+    "com.beachape"       %% "enumeratum"                            % "1.7.4",
     "com.beachape"       %% "enumeratum-play-json"                  % "1.8.1" excludeAll (jacksonAndPlayExclusions *),
     "org.codehaus.janino" % "janino"                                % "3.1.12" // it's required by logback for conditional logging
   )
@@ -29,13 +29,13 @@ object AppDependencies {
     "uk.gov.hmrc"       %% "bootstrap-test-play-30"  % bootstrapVersion,
     "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-30" % hmrcMongoVersion,
     "org.scalatest"     %% "scalatest"               % "3.2.19",
-    "com.vladsch.flexmark"   %  "flexmark-all"       % "0.64.8"             ,
     "org.scalatestplus" %% "scalacheck-1-15"         % "3.2.11.0",
     "org.scalatestplus" %% "mockito-3-4"             % "3.2.10.0",
-    "org.typelevel"     %% "cats-core"               % "2.13.0",
-    "org.scalacheck"    %% "scalacheck"              % "1.18.1",
+    "org.typelevel"     %% "cats-core"               % "2.12.0",
+    "org.scalacheck"    %% "scalacheck"              % "1.18.0",
+    "org.pegdown"        % "pegdown"                 % "1.6.0",
     "org.mockito"       %% "mockito-scala"           % "1.17.37",
-    "org.scalamock"     %% "scalamock"               % "7.3.2"
+    "org.scalamock"     %% "scalamock"               % "5.2.0"
   ).map(_ % "test, it")
 
   def apply(): Seq[ModuleID] = compile ++ test
