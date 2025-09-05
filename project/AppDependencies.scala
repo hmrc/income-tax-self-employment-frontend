4d8d4c1ad5fb3f2bc3ff9038339652e15ccb4e5e
TarekAhmed-MG <173894783+TarekAhmed-MG@users.noreply.github.com>
2025-09-05 12:21:10 +0100

    MIPR-1979: Upgrade dependencies on the Submissions service

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.13.0"
-  private val hmrcMongoVersion = "2.6.0"
+  private val bootstrapVersion = "9.19.0"
+  private val hmrcMongoVersion = "2.7.0"
 
   val jacksonAndPlayExclusions: Seq[InclusionRule] = Seq(
     ExclusionRule(organization = "com.fasterxml.jackson.core"),
@@ -15,7 +15,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"            % "12.6.0",
+    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"            % "12.10.0",
     "uk.gov.hmrc"        %% "play-conditional-form-mapping-play-30" % "3.3.0",
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
@@ -35,7 +35,7 @@ object AppDependencies {
     "org.typelevel"       %% "cats-core"               % "2.13.0",
     "org.scalacheck"      %% "scalacheck"              % "1.18.1",
     "org.mockito"         %% "mockito-scala"           % "2.0.0",
-    "org.scalamock"       %% "scalamock"               % "7.3.2"
+    "org.scalamock"       %% "scalamock"               % "7.4.1"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
