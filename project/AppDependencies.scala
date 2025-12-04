08eab976693bdb2c1a3e24c65c7d4b7b7158b059
Chrissystk <18244373+Chrissystk@users.noreply.github.com>
2025-12-04 09:47:27 +0000

    MIPR-2273 updated dependencies

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,8 +2,8 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "10.3.0"
-  private val hmrcMongoVersion = "2.10.0"
+  private val bootstrapVersion = "10.4.0"
+  private val hmrcMongoVersion = "2.11.0"
 
   val jacksonAndPlayExclusions: Seq[InclusionRule] = Seq(
     ExclusionRule(organization = "com.fasterxml.jackson.core"),
@@ -15,8 +15,8 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     play.sbt.PlayImport.ws,
-    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"            % "12.17.0",
-    "uk.gov.hmrc"        %% "play-conditional-form-mapping-play-30" % "3.3.0",
+    "uk.gov.hmrc"        %% "play-frontend-hmrc-play-30"            % "12.22.0",
+    "uk.gov.hmrc"        %% "play-conditional-form-mapping-play-30" % "3.4.0",
     "uk.gov.hmrc"        %% "bootstrap-frontend-play-30"            % bootstrapVersion,
     "uk.gov.hmrc.mongo"  %% "hmrc-mongo-play-30"                    % hmrcMongoVersion,
     "org.typelevel"      %% "cats-core"                             % "2.13.0",
@@ -35,7 +35,7 @@ object AppDependencies {
     "org.typelevel"       %% "cats-core"               % "2.13.0",
     "org.scalacheck"      %% "scalacheck"              % "1.19.0",
     "org.mockito"         %% "mockito-scala"           % "2.0.0",
-    "org.scalamock"       %% "scalamock"               % "7.5.0"
+    "org.scalamock"       %% "scalamock"               % "7.5.2"
   ).map(_ % Test)
 
   def apply(): Seq[ModuleID] = compile ++ test
